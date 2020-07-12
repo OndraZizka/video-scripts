e36ff13dc72100ca019a5645c6f059264433813e
Ondrej Zizka <zizka@seznam.cz>
2020-07-12 02:23:48 +0200

    Fix default tile height to 240 for tiled preview

--- FfmpegVideoPreviewsGenerator/generatePreviewForThisFolder.sh
+++ FfmpegVideoPreviewsGenerator/generatePreviewForThisFolder.sh
@@ -5,11 +5,11 @@ SCRIPT_DIR=$(dirname $(realpath $0))
 
 FILMS_DIR=${1}
 
-if [ "" == "$FILMS_DIR" ] ; then echo "Usage: $0 <dirWithMovies> [tilesX=6] [tilesY=6] [height=240]"; exit 1; fi
+if [ "" == "$FILMS_DIR" ] ; then echo "Usage: $0 <dirWithMovies> [tilesX=12] [tilesY=12] [height=240]"; exit 1; fi
 
 TILES_X="${2:-12}"
 TILES_Y="${3:-12}"
-TILE_HEIGHT="${4:-120}"
+TILE_HEIGHT="${4:-240}"
 
 find "$FILMS_DIR" -iname '*.mp4' -o -iname '*.avi' -o -iname '*.mkv' -o -iname '*.webm' -o -iname '*.mov' | sort | \
   xargs -rl -I {}  "$SCRIPT_DIR/videoPreviewGenerator.sh" "{}" "$TILE_HEIGHT" "$TILES_X" "$TILES_Y"
